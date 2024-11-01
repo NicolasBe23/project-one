c7dd68780e7ede0dc90f254c00639666e19621e8
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2024-11-01 17:09:23 +0000

    Update

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function addToCart(name, price) {
     const existingItem = cart.find((item) => item.name === name);
 
     if (existingItem) {
-        existingItem.quantity += 1;
+        existingItem.quantity += 1; 
     } else {
         cart.push({
             name,
