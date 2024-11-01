70b7028712e5baf5ae285a390f9e518a79f43aa5
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2024-11-01 16:58:23 +0000

    Update

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;
+    return hora >= 16 && hora < 22;
 }
 
 const spanItem = document.getElementById("date-span");
